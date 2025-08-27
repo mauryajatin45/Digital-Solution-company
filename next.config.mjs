0843c0ca28b6f55edf79eae71d975c6e272e0713
Jatin Maurya <mauryajatin45@gmail.com>
2025-08-27 13:37:08 +0530

    Add redirect configuration for root path to '/da'

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/',        // when someone visits "/"
+        destination: '/da', // redirect them to "/da"
+        permanent: true,    // true = 308 redirect, false = 307 redirect
+      },
+    ]
+  },
+};
 
 export default nextConfig;
